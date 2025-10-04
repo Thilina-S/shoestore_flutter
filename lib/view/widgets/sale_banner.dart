7368b747f37847d4d4767ba84fdaaf0ed4cc256e
thilinasandamal <thilinasandamal61@gmail.com>
2025-10-05 00:27:14 +0530

    sale banner part

--- lib/view/widgets/sale_banner.dart
+++ lib/view/widgets/sale_banner.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:shoestore/utils/app_textstyles.dart';
 
 class SaleBanner extends StatelessWidget {
   const SaleBanner({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Get yours',
+                  style: AppTextStyle.withColor(AppTextStyle.h3, Colors.white),
+                ),
+
+                Text(
+                  'Special Sale',
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.withWeight(AppTextStyle.h2, FontWeight.bold),
+                    Colors.white,
+                  ),
+                ),
+                Text(
+                  'Up to 50% off',
+                  style: AppTextStyle.withColor(AppTextStyle.h3, Colors.white),
+                ),
+              ],
+            ),
+          ),
+
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            ),
+
+            child: Text('Shop Now', style: AppTextStyle.buttonMedium),
+          ),
+        ],
+      ),
+    );
   }
 }
