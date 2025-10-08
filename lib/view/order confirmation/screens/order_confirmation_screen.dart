57bbb81fc18f288757ae9a545bb66fe33b8c958e
thilinasandamal <thilinasandamal61@gmail.com>
2025-10-08 12:39:59 +0530

    completed the all the project

--- lib/view/order confirmation/screens/order_confirmation_screen.dart
+++ lib/view/order confirmation/screens/order_confirmation_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shoestore/utils/app_textstyles.dart';
+import 'package:shoestore/view/home_screen.dart';
+import 'package:shoestore/view/main_screen.dart';
+import 'package:shoestore/view/my%20orders/view/screeens/my_orders_screen.dart';
 
 class OrderConfirmationScreen extends StatelessWidget {
   final String orderNumber;
@@ -14,6 +20,78 @@ class OrderConfirmationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
 
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                'assets/animations/order_success.json',
+                width: 200,
+                height: 200,
+                repeat: false,
+              ),
+              const SizedBox(height: 32),
+              Text(
+                'Order Confirmed !',
+                textAlign: TextAlign.center,
+                style: AppTextStyle.withColor(
+                  AppTextStyle.h3,
+                  isDark ? Colors.white : Colors.black,
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              Text(
+                'Your order #$orderNumber has been successfully placed.',
+                textAlign: TextAlign.center,
+                style: AppTextStyle.withColor(
+                  AppTextStyle.bodyLarge,
+                  isDark ? Colors.grey[400]! : Colors.grey[600]!,
+                ),
+              ),
+              const SizedBox(height: 48),
+              ElevatedButton(
+                onPressed: () {
+                  Get.to(() => MyOrdersScreen());
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 48,
+                    vertical: 16,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Track Order',
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.buttonMedium,
+                    Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  Get.offAll(() => const MainScreen());
+                },
+                child: Text(
+                  'Continue Shopping',
+                  style: AppTextStyle.withColor(
+                    AppTextStyle.buttonMedium,
+                    Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
