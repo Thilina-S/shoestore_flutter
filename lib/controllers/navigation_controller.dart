5878384e1e6459a84d3edb7c9eafee13551ac7ad
thilinasandamal <thilinasandamal61@gmail.com>
2025-11-07 12:22:18 +0530

    explain the navigation controller

--- lib/controllers/navigation_controller.dart
+++ lib/controllers/navigation_controller.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 
 class NavigationController extends GetxController {
+  //Rxint is a reactive integer variable to hold the current index of the navigation bar
   RxInt currentIndex = 0.obs;
 
+  //when the user taps on a navigation item, this method updates the current index / change current index
   void changeIndex(int index) {
     currentIndex.value = index;
   }
